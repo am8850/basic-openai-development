89cf0260c19240d96039c524c85caffd19be18ca
Alex Morales <am8850mia@gmail.com>
2023-08-07 16:27:48 -0400

    Added simples csharp project

---
+++ csharp/simplest/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+// dotnet add package Azure.AI.OpenAI --prerelease
+// dotnet add package dotenv.net
+// rename .env-example to .env and add your endpoint and key
+
+using Azure;
+using Azure.AI.OpenAI;
+using dotenv.net;
+
+// Load the full endpoint and key from the .env file
+DotEnv.Load();
+var endpoint = Environment.GetEnvironmentVariable("DAVINCI_OPENAI_ENDPOINT");
+var modelName = Environment.GetEnvironmentVariable("DAVINCI_OPENAI_DEPLOYMENT_NAME");
+var apiKey = Environment.GetEnvironmentVariable("DAVINCI_OPENAI_KEY");
+
+if (endpoint is null || apiKey is null || endpoint == "" || apiKey == "" || modelName is null || modelName == "")
+{
+    Console.WriteLine("Please add your model name, endpoint and key to the .env file");
+    return;
+}
+
+Console.WriteLine($"Using deployment at: {endpoint} with key {apiKey.Substring(0, 5)}...");
+
+// Create an SDK client
+OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(apiKey));
+
+// Prepare the prompt
+string prompt = "When was Microsoft founded?";
+Console.Write($"Prompt:\n\n{prompt}\n\n");
+
+// Get a completion
+Response<Completions> completionsResponse = await client.GetCompletionsAsync(modelName, prompt);
+string completion = completionsResponse.Value.Choices[0].Text;
+Console.WriteLine($"Completion Text: {completion}");
---
+++ csharp/simplest/README.md
@@ -0,0 +1,21 @@
+# Simplest
+
+## Running
+
+- Rename the `.env-sample` file to `.env`
+- Add the OpenAI endpoint name, endpoint uri, and the api key
+> **Note:** The endpoint only includes the host name and not the full URL
+- Type: `dotnet run .`
+
+## Expected output
+
+```text
+Using deployment at: https://eualemor.openai.azure.com/ with key bd987...
+Prompt:
+
+When was Microsoft founded?
+
+Completion Text: 
+
+Microsoft was founded on April 4, 1975.
+```
